d561e92b786105bbb63c14147f0318f44b7b3360
Asher Tedford <30579543+MystikFruit@users.noreply.github.com>
2018-02-25 16:05:45 -0800

    HappyPet, SleepyHead

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -8,7 +8,8 @@ public class HappyPet {
 	static int HL = 0;
 	static int FL = 0;
 	static int FL2 = 0;
-	static int FL3 = 0;
+	static int WL = 0;
+
 	public static void main(String[] args) {
 		JOptionPane.showOptionDialog(null, "What kind of pet do you want to own?", null, 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Dog", "Cat", "Rock", "Sloth", "Lamp", "Iguana",
@@ -18,7 +19,7 @@ public class HappyPet {
 		for (int i = 0; i < 999999999; i++) {
 			int task = JOptionPane.showOptionDialog(null, "What do you want to do with your pet?", null, 0,
 					JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Feed", "Walk", "Pet", "Ignore", "Check Happiness Level" }, null);
+					new String[] { "Feed", "Walk", "Pet", "Ignore", "Play", "Check Happiness Level" }, null);
 			if (task == 0) {
 				feed(name);
 			} else if (task == 1) {
@@ -28,10 +29,17 @@ public class HappyPet {
 			} else if (task == 3) {
 				ignore(name);
 			} else if (task == 4) {
+				play(name);
+			} else if (task == 5) {
 				checkHL(name);
 			}
-		}
+			if (HL >= 50) {
+				JOptionPane.showMessageDialog(null, name + "'s happiness level has reached its max!");
+				JOptionPane.showMessageDialog(null, name + " is going to go take a nap now."); break;
 
+			}
+
+		}
 	}
 
 	static void feed(String name) {
@@ -43,18 +51,21 @@ public class HappyPet {
 		} else {
 			if (FL == 2) {
 				JOptionPane.showMessageDialog(null, name + " isn't hungry anymore.");
-				FL3 +=1;
-			}
-			if (FL3 >= 2) {
-				JOptionPane.showMessageDialog(null, name + " is still full.");
 			}
 		}
 	}
 
 	static void walk(String name) {
-		JOptionPane.showMessageDialog(null, "You have taken " + name + " for a walk.");
-		JOptionPane.showMessageDialog(null, name + "'s happiness level has gone up by 10!");
-		HL += 10;
+		if (WL < 2) {
+			WL += 1;
+			JOptionPane.showMessageDialog(null, "You have taken " + name + " for a walk.");
+			JOptionPane.showMessageDialog(null, name + "'s happiness level has gone up by 10!");
+			HL += 10;
+		} else {
+			if (WL == 2) {
+				JOptionPane.showMessageDialog(null, name + " doesn't want to take another walk.");
+			}
+		}
 	}
 
 	static void pet(String name) {
@@ -65,7 +76,7 @@ public class HappyPet {
 
 	static void ignore(String name) {
 		JOptionPane.showMessageDialog(null, name + " doesn't like to be ignored.");
-		JOptionPane.showConfirmDialog(null, name + "'s happiness level has gone down by 2");
+		JOptionPane.showMessageDialog(null, name + "'s happiness level has gone down by 2");
 		HL -= 2;
 	}
 
@@ -73,4 +84,9 @@ public class HappyPet {
 		JOptionPane.showMessageDialog(null, name + "'s happiness level is currently at " + HL);
 	}
 
+	static void play(String name) {
+		JOptionPane.showMessageDialog(null, "You have played with " + name + ".");
+		JOptionPane.showMessageDialog(null, name + "'s happiness has gone up by 4!");
+		HL += 4;
+	}
 }
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,28 +1,34 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /*
-         * Ask the user for these values using
-         * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-         * JOptionPane.YES_NO_OPTION);
-         */
-    
+		int isWeekday, isVacation;
 
-
-        /*
-         * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!” If it is a weekday, and we are on vacation,
-         * print “sleep in”.
-         */
-    }
+		/*
+		 * Ask the user for these values using JOptionPane.showConfirmDialog(null,
+		 * "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		 */
+		isWeekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		if (isWeekday == 0) {
+			isVacation = JOptionPane.showConfirmDialog(null, "Is it a Vacation?", "", JOptionPane.YES_NO_OPTION);
+			if (isVacation == 0) {
+				JOptionPane.showMessageDialog(null, "Then sleep in.");
+			} else {
+				JOptionPane.showMessageDialog(null, "Then get up lazybones!");
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Then sleep in.");
+		}
+		/*
+		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!” If it is a weekday, and we are on vacation, print “sleep
+		 * in”.
+		 */
+	}
 }
